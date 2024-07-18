265617f55faaa9b38abc9a17167acc96e11b34ff
deepjoy9 <deepjoysarkar9@gmail.com>
2024-07-18 22:22:04 +0530

    Add task input section and keyboard handling

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,51 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import Task from "../components/Task";
+import {
+  GestureHandlerRootView,
+  TextInput,
+} from "react-native-gesture-handler";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      {/* Today's Tasks */}
-      <View style={styles.tasksWrapper}>
-        <Text style={styles.sectionTitle}>Today's tasks</Text>
-        <View style={styles.items}>
-          {/* All tasks */}
-          <Task text={"Task 1"} />
-          <Task text={"Task 2"} />
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        {/* Today's Tasks */}
+        <View style={styles.tasksWrapper}>
+          <Text style={styles.sectionTitle}>Today's tasks</Text>
+          <View style={styles.items}>
+            {/* All tasks */}
+            <Task text={"Task 1"} />
+            <Task text={"Task 2"} />
+          </View>
         </View>
+
+        {/* Write a task */}
+        {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.writeTaskWrapper}
+        >
+          <TextInput
+            style={styles.input}
+            placeholder={"Write a task"}
+            placeholderTextColor="#888"
+          />
+          <TextInput />
+          <TouchableOpacity>
+            <View style={styles.addWrapper}>
+              <Text>+</Text>
+            </View>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
       </View>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
@@ -32,5 +64,32 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
   },
 });
